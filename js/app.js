c90f2eaafe33185d19a9b74ee6b98553be86bb18
ric <ric@snapzio.com>
2016-08-31 09:14:09 +0800

    added react preset

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ var ReactDOM = require("react-dom");
 
 var Hello = React.createClass({
   render: function() {
-    return React.createElement("h1", null, "Hello React!");
+    // return React.createElement("h1", null, "Hello React!");
+    return (<h3>Helo Webpack</h3>)
   }
 });
 
