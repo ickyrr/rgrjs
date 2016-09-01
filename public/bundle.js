000188815091cf89ab4cfe7cc72b670a72bc5e8a
ric <ric@snapzio.com>
2016-09-01 08:30:35 +0800

    added mongodb collection

--- public/bundle.js
+++ public/bundle.js
@@ -209,25 +209,40 @@
 	var cachedSetTimeout;
 	var cachedClearTimeout;
 
+	function defaultSetTimout() {
+	    throw new Error('setTimeout has not been defined');
+	}
+	function defaultClearTimeout() {
+	    throw new Error('clearTimeout has not been defined');
+	}
 	(function () {
 	    try {
-	        cachedSetTimeout = setTimeout;
+	        if (typeof setTimeout === 'function') {
+	            cachedSetTimeout = setTimeout;
+	        } else {
+	            cachedSetTimeout = defaultSetTimout;
+	        }
 	    } catch (e) {
-	        cachedSetTimeout = function cachedSetTimeout() {
-	            throw new Error('setTimeout is not defined');
-	        };
+	        cachedSetTimeout = defaultSetTimout;
 	    }
 	    try {
-	        cachedClearTimeout = clearTimeout;
+	        if (typeof clearTimeout === 'function') {
+	            cachedClearTimeout = clearTimeout;
+	        } else {
+	            cachedClearTimeout = defaultClearTimeout;
+	        }
 	    } catch (e) {
-	        cachedClearTimeout = function cachedClearTimeout() {
-	            throw new Error('clearTimeout is not defined');
-	        };
+	        cachedClearTimeout = defaultClearTimeout;
 	    }
 	})();
 	function runTimeout(fun) {
 	    if (cachedSetTimeout === setTimeout) {
 	        //normal enviroments in sane situations
+	        return setTimeout(fun, 0);
+	    }
+	    // if setTimeout wasn't available but was latter defined
+	    if ((cachedSetTimeout === defaultSetTimout || !cachedSetTimeout) && setTimeout) {
+	        cachedSetTimeout = setTimeout;
 	        return setTimeout(fun, 0);
 	    }
 	    try {
@@ -246,6 +261,11 @@
 	function runClearTimeout(marker) {
 	    if (cachedClearTimeout === clearTimeout) {
 	        //normal enviroments in sane situations
+	        return clearTimeout(marker);
+	    }
+	    // if clearTimeout wasn't available but was latter defined
+	    if ((cachedClearTimeout === defaultClearTimeout || !cachedClearTimeout) && clearTimeout) {
+	        cachedClearTimeout = clearTimeout;
 	        return clearTimeout(marker);
 	    }
 	    try {
--- server.js
+++ server.js
@@ -1,7 +1,34 @@
 import express from 'express';
+import {MongoClient} from 'mongodb';
 
 let app = express();
 
 app.use(express.static("public"));
 
-app.listen(3000);
+let url;
+
+if(process.env.MONGO_URL) {
+	url = process.env.MONGO_URL;
+}
+
+url = "mongodb://localhost:27017/rgrjs";
+
+let db;
+
+MongoClient.connect(url, (err, database) => {1
+	if(err) throw err;
+
+	db = database;
+
+	app.listen(3000, () => console.log('Now connected to ' + url));
+
+});
+
+app.get('/links', (req, res) => {
+
+	db.collection("links").find({}).toArray( (err, links) => {
+		if(err) throw err;
+		res.json(links);
+	});
+
+});
