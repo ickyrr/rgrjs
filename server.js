1f5fd18edeae19c2dffbe99a7a3dfa8be0728d06
ric <ric@snapzio.com>
2016-09-05 09:11:13 +0800

    started with grapqhl

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import express from 'express';
 import {MongoClient} from 'mongodb';
+import schema from './data/schema';
+import GraphQLHTTP from 'express-graphql';
 
 let app = express();
 
 app.use(express.static("public"));
+
+app.use("/graphql", GraphQLHTTP({
+	schema,
+	graphiql: true,
+}));
 
 let url;
 
